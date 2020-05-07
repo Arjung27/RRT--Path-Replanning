be71b673712d5e490808f6cd9e5651fac35fa3b8
Aruna Baijal <aruna11baijal@gmail.com>
2020-05-06 22:40:46 -0400

    Custom condition to make sure trajectories cross

--- RRTStar/rrt_star.py
+++ RRTStar/rrt_star.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
                 "/../RRT/")
-
+np.random.seed(25)
 try:
     from rrt import RRT
 except ImportError:
@@ -35,11 +35,11 @@ class RRTStar(RRT):
             self.cost = 0.0
 
     def __init__(self, start, goal, obstacle_list_circle, obstacle_list_square, rand_area,
-                 expand_dis=30.0,
+                 expand_dis=3.0,
                  path_resolution=1.0,
                  goal_sample_rate=20,
                  max_iter=500,
-                 connect_circle_dist=50.0,
+                 connect_circle_dist=10.0,
                  clearance=0,
                  detection_range=2.0):
         super().__init__(start, goal, obstacle_list_circle, obstacle_list_square,
@@ -55,6 +55,7 @@ class RRTStar(RRT):
         """
         self.connect_circle_dist = connect_circle_dist
         self.goal_node = self.Node(goal[0], goal[1])
+        self.detection_range = detection_range
 
     def planning(self, animation=True, search_until_max_iter=True):
         """
@@ -194,7 +195,7 @@ class RRTStar(RRT):
         y_final = y[indexes]
         func_trajectory = interp1d(x_final, y_final, kind='cubic')
 
-        return func_trajectory(x_final)
+        return [x_final, func_trajectory(x_final)]
 
     def check_collision_obstacle(self, x_coord, y_coord):
 
@@ -215,29 +216,35 @@ class RRTStar(RRT):
         return True
 
     def get_obstacle_location(self):
-        #TODO
-        # points of spline.
         path = self.generate_obstacle_trajectory()
-        
+        # print("OBSTACLE PATHHHH")
+        # print(path)
+        f = open("obstaclePath.txt", "a+")
         global obs_x
         global obs_y
-        for pts in path:
-            obs_x = pts[0]
-            obs_y = pts[1]
+        for i in range(len(path[0])):
+            obs_x = path[0][i]
+            obs_y = path[1][i]
+            print("Obstacle Path: " + str(obs_x) + ', ' + str(obs_y).format(threading.current_thread().name))
+            f.write(str(obs_x) + ',' + str(obs_y) + '\n')
             time.sleep(1)
+        f.close()
         # return self.Node(0, 0)
     
     def check_obstacle_in_range(self, current_node, obstacle_node):
         d, _ = self.calc_distance_and_angle(current_node, obstacle_node)
         if d < self.detection_range:
+            print("Obstacle within range....")
             new_node = self.steer(current_node, obstacle_node)
             is_path_blocked = self.check_collision(new_node, self.obstacle_list_circle, self.obstacle_list_square, self.clearance)
-            if not is_path_blocked:
+            if is_path_blocked:
+                print("Obstacle visible....")
                 return True
         return False
     
     def normalize_angle(self, angle):
-        newAngle = angle
+        newAngle = np.rad2deg(angle)
+        print(newAngle)
         while newAngle <= -180:
             newAngle += 360
         while newAngle > 180:
@@ -245,32 +252,42 @@ class RRTStar(RRT):
         return newAngle
     
     def check_trajectory_collision(self, current_node, obstacle_node, old_obstacle_node):
-        threshold_angle = 15
+        threshold_angle = 140
         angle_direction = math.atan2((obstacle_node.y - current_node.y), (obstacle_node.x - current_node.x))
+        print("Angle direction ", angle_direction)
         angle_obs = math.atan2((obstacle_node.y - old_obstacle_node.y), (obstacle_node.x - old_obstacle_node.x))
+        print("Obstacle velocity angle ", angle_obs)
         angle_robot = math.atan2((current_node.y - current_node.parent.y), (current_node.x - current_node.parent.x))
+        print("Robot velocity angle ", angle_robot)
         if abs(self.normalize_angle(angle_direction - angle_robot)) < threshold_angle:
             angle_v_diff = abs(self.normalize_angle(angle_robot - angle_obs))
-            if angle_v_diff < threshold_angle:
-                return False
-            elif angle_v_diff > 180 - threshold_angle:
+            if angle_v_diff > 180 - threshold_angle:
+                print("Going to collide")
                 return True
-            else:
+            elif angle_v_diff < threshold_angle:
+                print("Option 1")
                 return False
-        
+            else:
+                print("Option 2")
+                return False
         
     def replan_if_path_blocked(self, current_node, obstacle_node, path):
+        print("Current location ", current_node.x, current_node.y)
+        print("Old obstacle location ", obstacle_node.x, obstacle_node.y)
         time.sleep(1)
         new_obstacle_node = self.Node(obs_x, obs_y)
+        print("New obstacle location ", new_obstacle_node.x, new_obstacle_node.y)
         
         if self.check_trajectory_collision(current_node, new_obstacle_node, obstacle_node):
             #TODO
             #Replan algorithm
-            path = self.replan()
+            print("REPLANNING REQUIRED")
+            path = path
         
         return path
     
     def need_for_replan(self, path):
+        time.sleep(39)
         final_path = []
         nodes_to_visit = deque(path)
         prev_node = None
@@ -280,18 +297,19 @@ class RRTStar(RRT):
             final_path.append(robot)
             current_node = self.Node(robot[0], robot[1])
             current_node.parent = prev_node
+            print("Robot Path: " + str(current_node.x) + ', ' + str(current_node.y).format(threading.current_thread().name))
+            if prev_node is not None:
+                print("Robot Parent: " + str(current_node.parent.x) + ', ' + str(current_node.parent.y).format(threading.current_thread().name))
             if self.check_obstacle_in_range(current_node, obstacle_node):
                 new_path = self.replan_if_path_blocked(current_node, obstacle_node, nodes_to_visit)
                 nodes_to_visit = deque(new_path)
             prev_node = current_node
-                
+            time.sleep(1)
+
+        f = open("nodeReplannedPath.txt", "a+")
         for step in final_path:
-            f = open("nodeReplannedPath.txt", "a+")
-            # toWrite = str([self.path_resolution * math.cos(theta), self.path_resolution * math.sin(theta)
-            #                   , theta])
-            toWrite = str([step[0], step[1], step[2]])
-            f.write(toWrite[1:len(toWrite) - 1] + '\n')
-            f.close()
+            f.write(str(step[0]) + ',' + str(step[1]) + ',' + str(step[2]) + '\n')
+        f.close()
 
 def main():
     print("Start " + __file__)
@@ -336,44 +354,69 @@ def main():
                        clearance=clearance+radius)
     open('nodePath.txt', 'w').close()
     open('nodeReplannedPath.txt', 'w').close()
+    open('obstaclePath.txt', 'w').close()
     # with concurrent.futures.ThreadPoolExecutor() as executor:
     #     t1 = executor.submit(rrt_star.planning, show_animation)
     #     path = t1.result()
-    path = rrt_star.planning(animation=show_animation)
+    # path = rrt_star.planning(animation=show_animation)
+    # path = [[6, 10, None], [4.935288207869231, 8.167564596777668, 1.03330664866742], [3.399344460620826, 5.590574723827343, 1.216212269993973], [3.0521441331220442, 4.6527837382741986, 2.095447056264107], [4.053965617034213, 2.9217858427318557, 0.6658839055357819], [2.4812235054113425, 1.6862769393531567, 0.5969134076015434], [0, 0, 0]]
+    path = [[6, 10, None], [8.295166061988946, 9.647090488213937, 1.7064226267848028], [8.56558783411669, 7.665456802297366, 1.3988459609471349], [8.052275003299025, 4.709698027004978, 1.6670188814098872], [8.340497421582613, 1.72357548471058, 0.8111946307190926], [6.963232050695838, 0.27335477800552366, 0.18204962000928065], [4.996282682866677, -0.08873662536724658, 0.010189555964332997], [1.9964384220953, -0.11930476428721955, -0.05968781681993424], [0, 0, 0]]
+
+
+    f = open('nodePath.txt', 'r')
+    lines = f.readlines()
+    x_int = 0
+    y_int = 0
+    pts = []
+    pts.append([x_int, y_int])
+    for line in lines:
+        points = line.rstrip().split(',')
+        print(points)
+        pts.append([float(points[0]), float(points[1])])
     
-    t2 = threading.Thread(target=rrt_star.get_obstacle_location)
+    t1 = threading.Thread(target=rrt_star.get_obstacle_location, name='t1')
+    t2 = threading.Thread(target=rrt_star.need_for_replan, args=(path,), name='t2')
+
+    t1.start()
     t2.start()
     
-    t3 = threading.Thread(target=rrt_star.need_for_replan, args=(path,))
-    t3.start()
+    t1.join()
+    t2.join()
     
-
     if path is None:
         print("Cannot find path")
     else:
-        print("found path!!")
-        f = open('nodePath.txt', 'r')
-        lines = f.readlines()
-        x_int = 0
-        y_int = 0
-        pts = []
-        pts.append([x_int, y_int])
+        # print("found path!!")
+        # f = open('nodePath.txt', 'r')
+        # lines = f.readlines()
+        # x_int = 0
+        # y_int = 0
+        # pts = []
+        # pts.append([x_int, y_int])
         print(path)
         # Draw final path
+
+        f1 = open('obstaclePath.txt', 'r')
+        lines1 = f1.readlines()
+        # x_int = 0
+        # y_int = 0
+        pts_obs = []
+        # pts.append([x_int, y_int])
+        for line in lines1:
+            points = line.rstrip().split(',')
+            # print(points)
+            pts_obs.append([float(points[0]), float(points[1])])
+            
         if show_animation:
             rrt_star.draw_graph()
             plt.plot([x for (x, y, theta) in path], [y for (x, y, theta) in path], '-b')
-            for line in lines:
-                points = line.rstrip().split(',')
-                print(points)
-                
-                # plt.plot(float(points[0]), float(points[1]), '-r')
-                # print(points)
-                # x_int += float(points[0])
-                # y_int += float(points[1])
-                pts.append([float(points[0]), float(points[1])])
+            # for line in lines:
+            #     points = line.rstrip().split(',')
+            #     print(points)
+                # pts.append([float(points[0]), float(points[1])])
             
             plt.plot(np.asarray(pts)[:,0], np.asarray(pts)[:,1], '-r')
+            plt.plot(np.asarray(pts_obs)[:,0], np.asarray(pts_obs)[:,1], '-y')
             plt.grid(True)
             plt.pause(0.01)  # Need for Mac
             plt.show()
